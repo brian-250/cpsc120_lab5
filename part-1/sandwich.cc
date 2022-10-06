5fc62975ba6ced4908b2834526be33ce6d61e1d2
Max Rivas <Rivas.max11@csu.fullerton.edu>
2022-10-06 14:26:05 -0700

    Creates a sandwich with 3 command line arguments

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -1,23 +1,32 @@
-// TODO: Add the required header
+// Max Rivas
+// CPSC 120-01
+// 2022-10-06
+// Rivas.max11@csu.fullerton.edu
+// @Mrivas0331
+//
+// Lab 05-01
+// Partners: @brian-250
+//
+// Create sandwich
+//
 
 #include <iostream>
 #include <string>
 #include <vector>
-
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments(argv, argv + argc);
 
-  // TODO: Validate that the number of arguments is correct.
-  // If not, print an error message and return a non-zero value.
+  if (arguments.size() != 4) {
+    std::cout << "error: you must supply three arguments";
+    return 1;
+  } 
+    std::string protein{arguments[1]};
+    std::string bread{arguments[2]};
+    std::string condiment{arguments[3]};
 
-  // TODO: Declare three std::string variables to hold the
-  // protein, bread, and condiment input.
-  // Initialize each variable with an element of the arguments vector
-  // declared above.
-
-  // TODO: Use cout to print output following the pattern
-  // Your order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
-  // on its own line.
+    std::cout << "Your order:\nA " << protein << " sandwich on " << bread
+              << " with " << condiment << ".";
+  
 
   return 0;
 }
